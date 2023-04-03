30a0d55511087d134e902ac5590b2bf2189a380f
sbpark88 <saebyulpark88@gmail.com>
2023-04-03 23:46:26 +0900

    Update: Make the open/close labels work
    Proxy 제대로 작동 되는게 맞나...?

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import {insertHtmlAfterBegin, insertHtmlBeforeEnd} from "./util/UI/ManagingDOM"
 import {pipe} from "./util/FP";
 
 const app = document.getElementById('app');
+let issueList = [];
 
 // MARK: page init
 // init
@@ -12,10 +13,58 @@ const app = document.getElementById('app');
   insertHtmlAfterBegin(app)(mainViewComponent)
 })()
 
+const nonFilteredIssueList = await getIssue()
+issueList = nonFilteredIssueList.filter(issue => 'open' === issue.status)
+
 // MARK: issue list
 const ul = document.querySelector("#issues-wrapper > div.issue-list.flex.ml-auto > ul")
+const createIssueHtml = list => list.map(issue => getIssueItemTpl(issue))
+const clearBeforeRender = el => () => el.innerHTML = ''
+const clearIssueBeforeRender = clearBeforeRender(ul)
+const renderIssue = el => html => insertHtmlBeforeEnd(el)(html)
+const renderIssueAtUl = renderIssue(ul)
 
-const issueList = await getIssue()
-const issueHtml = issueList.map(issue => getIssueItemTpl(issue))
-insertHtmlBeforeEnd(ul)(issueHtml)
+const renderIssueList = pipe(
+    createIssueHtml,
+    renderIssueAtUl
+)
 
+// MARK: counter label
+const findStatusTabDom = () => {
+  const statusTab = document.getElementsByClassName('statusTab')[0];
+  const statusOpen = statusTab.getElementsByClassName('open-count')[0];
+  const statusClosed = statusTab.getElementsByClassName('close-count')[0];
+  return {statusTab, statusOpen, statusClosed}
+}
+
+const {statusTab, statusOpen, statusClosed} = findStatusTabDom()
+
+const status = Object.freeze({
+  open: 'open',
+  close: 'close'
+})
+const statusFilter = status => list => list.filter(issue => status === issue.status)
+const openCounter = statusFilter(status.open)(nonFilteredIssueList).length ?? 0
+const closedCounter = statusFilter(status.close)(nonFilteredIssueList).length ?? 0
+
+const handler = {
+  set(target, property, value) {
+    target = value;
+    statusOpen.textContent = `${openCounter} Opens`
+    statusClosed.textContent = `${closedCounter} Closed`
+    clearIssueBeforeRender()
+    renderIssueList(target)
+    return true
+  }
+}
+
+const proxy = new Proxy(issueList, handler)
+proxy.target = issueList
+
+statusOpen.addEventListener('click', async () => {
+  // proxy.target = await getIssue().then(res => res.filter(issue => 'open' === issue.status))
+  proxy.target = nonFilteredIssueList.filter(issue => 'open' === issue.status)
+})
+statusClosed.addEventListener('click', async () => {
+  proxy.target = nonFilteredIssueList.filter(issue => 'close' === issue.status)
+})
