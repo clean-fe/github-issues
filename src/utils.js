ccebd2f8ade302ef54b81162c8e44e7f66d08238
sihyeon lee <hey210056@gmail.com>
2023-04-04 23:59:20 +0900

    refactor: 변수명 변경

--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,11 @@ export const $ = (selector) => document.querySelector(selector);
 export const pipe =
   (...functions) =>
   (args) =>
-    functions.reduce((arg, fn) => fn(arg), args);
+    functions.reduce((arg, funs) => funs(arg), args);
 
 export const fetchList = async (url) => {
   const res = await fetch(`${url}`);
-  const listRes = await res.json();
+  const list = await res.json();
 
-  return listRes;
+  return list;
 };
