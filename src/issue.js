ea235300a3785df2bd11c42e225215278ce82b87
Jiwon Kim <lost731@naver.com>
2023-04-04 22:44:56 +0900

    feat: 버튼 토글 구현 및 시퀀스 적용

--- src/issue.js
+++ src/issue.js
@@ -9,6 +9,17 @@ const pipe =
   (initialValue) =>
     fns.reduce((acc, fn) => fn(acc), initialValue);
 
+const sequence = (...fns) => {
+  fns.forEach((fn) => fn());
+};
+
+// TODO: 재사용 가능
+const $ = (selectorName) => document.querySelector(selectorName);
+
+const setTpl = (html) => ($dom) => {
+  $dom.innerHTML = html;
+};
+
 const request = async (url) => {
   const res = await fetch(url);
   return res.json();
@@ -19,22 +30,34 @@ const filterStatus = (status) => async (data) => {
   return issueList.filter((item) => item.status === status);
 };
 
-const $ = (selectorName) => document.querySelector(selectorName);
-const setTpl = (html) => ($dom) => {
-  $dom.innerHTML = html;
+const getOpenStatusList = await pipe(request, filterStatus('open'))(issueUrl);
+const getCloseStatusList = await pipe(request, filterStatus('close'))(issueUrl);
+
+const setIssueTpl = setTpl(getIssueTpl(getOpenStatusList.length, getCloseStatusList.length));
+
+const setIssueListTpl = (list) => setTpl(list.reduce((acc, curr) => (acc += getIssueItemTpl(curr)), ''));
+
+const setOpenIssueListTpl = setIssueListTpl(getOpenStatusList);
+const setCloseIssueListTpl = setIssueListTpl(getCloseStatusList);
+
+// TODO: 리팩터링(가독성)
+export const setIssueOnDocument = () => {
+  sequence(
+    () => setIssueTpl($('#app')),
+    () => setOpenIssueListTpl($('#issues')),
+    () => addToggleCountEvent(setCloseIssueListTpl, $('.close-count'), $('.open-count')),
+    () => addToggleCountEvent(setOpenIssueListTpl, $('.open-count'), $('.close-count'))
+  );
 };
 
-const getOpenStatus = await pipe(request, filterStatus('open'))(issueUrl);
-const getCloseStatus = await pipe(request, filterStatus('close'))(issueUrl);
+const addToggleCountEvent = (setListTpl, $target, $nontarget) => {
+  $target.addEventListener('click', () => {
+    toggleCountBtn(setListTpl, $target, $nontarget);
+  });
+};
 
-const setIssueTpl = setTpl(getIssueTpl(getOpenStatus.length, getCloseStatus.length));
-
-const setIssueListTpl = setTpl(
-  getOpenStatus.reduce((acc, curr) => (acc += getIssueItemTpl(curr)), ''),
-);
-
-// TODO: 합성 함수로 변경
-export const setIssueOnDocument = () => {
-  setIssueTpl($('#app'));
-  setIssueListTpl($('#issues'));
+const toggleCountBtn = (setListTpl, $focused, $unfocused) => {
+  setListTpl($('#issues'));
+  $focused.style.fontWeight = 'bold';
+  $unfocused.style.fontWeight = 'normal';
 };
