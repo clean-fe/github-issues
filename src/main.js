c5dfa82744f782b89755d277738d72f556905d02
Autumn <autumn@inflab.com>
2023-04-04 20:20:48 +0900

    then -> async/await로 통일

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ app.innerHTML = getIssueTpl();
 const ul = document.querySelector('.issue-list ul');
 
 const getData = async () => {
-  await fetch('/data-sources/issues.json')
-    .then((response) => response.json())
-    .then((data) => {
-      const items = data.map((item) => getIssueItemTpl(item));
-      ul.innerHTML = items.join('');
-    });
+  const res = await fetch('/data-sources/issues.json')
+  const data = await res.json();
+
+  return data;
 };
 
-getData();
+const items = await getData();
+ul.innerHTML = items.map((item) => getIssueItemTpl(item)).join('');
+
+// open-count, close-count dom을 찾는다
+// items에서 opened, closed 를 각각 필터, length 구해서
+// 각각의 dom에 innerHTML 한다.
