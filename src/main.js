20355cf1bb2d43782e146eb67022c68b4a587371
sa02045 <sa02045@naver.com>
2023-04-06 00:36:46 +0900

    feat: 메인함수 정리

--- src/main.js
+++ src/main.js
@@ -1,49 +1,16 @@
-import { getIssueItemTpl, getIssueTpl } from "./tpl.js";
-import { fetchIssues } from "./api.js";
-import {
-  updateDOM,
-  fromEvent,
-  pipe,
-  filterItems,
-  renderItems,
-} from "./utils.js";
+import { getIssueTpl } from "./tpl.js";
+import { fromEvent, pipe } from "./utils.js";
+import { renderApp, renderIssues } from "./render.js";
+import { handleClickTab, handleInputSearch } from "./eventHandlers.js";
 
 main();
 
 function main() {
-  pipe(getIssueTpl, updateDOM("#app"), renderIssues("open"))();
+  pipe(
+    renderApp(getIssueTpl),
+    renderIssues((issue) => issue.status === "open")
+  )();
+
   fromEvent(document, "input").subscribe(handleInputSearch);
   fromEvent(document, "click").subscribe(handleClickTab);
-}
-
-function renderIssues(issueStatus) {
-  return pipe(
-    fetchIssues,
-    filterItems((item) => item.status === issueStatus),
-    renderItems(getIssueItemTpl),
-    updateDOM("ul")
-  );
-}
-
-function handleInputSearch(e) {
-  if (e.target.id === "filter-input") {
-    pipe(
-      fetchIssues,
-      filterItems((item) => item.title.includes(e.target.value)),
-      renderItems(getIssueItemTpl),
-      updateDOM("ul")
-    )();
-  }
-}
-
-function handleClickTab(e) {
-  if (e.target.classList.contains("open-count")) {
-    renderIssues("open")();
-    return;
-  }
-
-  if (e.target.classList.contains("close-count")) {
-    renderIssues("close")();
-    return;
-  }
 }
