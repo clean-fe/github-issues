e80abc90e003922d85f995f383d327820b3fb5c6
sihyeon lee <hey210056@gmail.com>
2023-04-05 14:10:44 +0900

    refactor: asyn/awiat pipe 함수 생성 및 shareParams 함수 생성

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,15 @@ export const $ = (selector) => document.querySelector(selector);
 export const pipe =
   (...functions) =>
   (args) =>
-    functions.reduce((arg, funs) => funs(arg), args);
+    functions.reduce((arg, func) => func(arg), args);
+
+export const promisePipe =
+  (...functions) =>
+  async (args) =>
+    functions.reduce(
+      async (arg, func) => func(await arg),
+      Promise.resolve(args)
+    );
 
 export const fetchList = async (url) => {
   const res = await fetch(`${url}`);
@@ -11,3 +19,8 @@ export const fetchList = async (url) => {
 
   return list;
 };
+
+export const shareParams =
+  (...functions) =>
+  (param) =>
+    functions.forEach((func) => func(param));
