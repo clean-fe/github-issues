ff9f68f4ed15df225ea0db5679a255fd263ef850
sa02045 <sa02045@naver.com>
2023-04-06 00:26:32 +0900

    feat: 검색기능 추가, 함수분리

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import { getIssueItemTpl, getIssueTpl } from "./tpl.js";
-import { Observable } from "./Observable.js";
-
-const ISSUES_URL = "../data-sources/issues.json";
+import { fetchIssues } from "./api.js";
+import {
+  updateDOM,
+  fromEvent,
+  pipe,
+  filterItems,
+  renderItems,
+} from "./utils.js";
 
 main();
 
 function main() {
   pipe(getIssueTpl, updateDOM("#app"), renderIssues("open"))();
+  fromEvent(document, "input").subscribe(handleInputSearch);
   fromEvent(document, "click").subscribe(handleClickTab);
 }
 
@@ -19,41 +25,15 @@ function renderIssues(issueStatus) {
   );
 }
 
-function filterItems(filterer) {
-  return (items) => items.filter(filterer);
-}
-
-function renderItems(renderer) {
-  return (items) => items.map(renderer);
-}
-
-function updateDOM(target) {
-  return (template) => {
-    document.querySelector(target).innerHTML = template;
-  };
-}
-
-async function fetchData(url) {
-  const res = await fetch(url);
-  return res.json();
-}
-
-async function fetchIssues() {
-  return fetchData(ISSUES_URL);
-}
-
-function pipe(...functions) {
-  return (initArg) =>
-    functions.reduce(
-      async (nextArg, nextFunction) => nextFunction(await nextArg),
-      initArg
-    );
-}
-
-function fromEvent(target, eventName) {
-  return new Observable((observer) => {
-    target.addEventListener(eventName, observer);
-  });
+function handleInputSearch(e) {
+  if (e.target.id === "filter-input") {
+    pipe(
+      fetchIssues,
+      filterItems((item) => item.title.includes(e.target.value)),
+      renderItems(getIssueItemTpl),
+      updateDOM("ul")
+    )();
+  }
 }
 
 function handleClickTab(e) {
