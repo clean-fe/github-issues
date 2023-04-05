512e6ce29cbcecd5514ee99b264ecb7f1d0c763e
김양원 <yangwon.kim@woowahan.com>
2023-04-06 07:54:12 +0900

    refactor: pipe 내부에서 Promise 객체 판단 및 처리 로직 추가

--- src/utils/fn.js
+++ src/utils/fn.js
@@ -1,6 +1,6 @@
 export const pipe =
   (...fns) =>
   (initialValue) =>
-    fns.reduce((acc, fn) => fn(acc), initialValue);
+    fns.reduce((acc, fn) => (acc instanceof Promise ? acc.then(fn) : fn(acc)), initialValue);
 
 export const go = (a, ...fns) => fns.reduce((acc, fn) => fn(acc), a);
