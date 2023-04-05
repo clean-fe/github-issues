898455a1c1e5764ab25b59d29f358c284122dfae
김양원 <yangwon.kim@woowahan.com>
2023-04-05 20:44:39 +0900

    feat: 데이터 mapper 함수 생성하여 pipe에 적용

--- src/issue.js
+++ src/issue.js
@@ -9,22 +9,29 @@ const filterStatus = (status) => async (data) => {
   return issueList.filter((item) => item.status === status);
 };
 
-const mapIssue = (data) => {
-  // title, tags(tagName, color), _id, status, open-date, milestones
+const mapIssue = async (data) => {
+  const issueList = await data;
+  return issueList.map(({ title, tags, _id, status, openDate, milestones }) => ({
+    title,
+    tags,
+    _id,
+    status,
+    openDate,
+    milestones,
+  }));
 };
 
-const openStatusList = await pipe(request, filterStatus('open'))(ISSUE_URL);
-const closeStatusList = await pipe(request, filterStatus('close'))(ISSUE_URL);
+const openStatusList = await pipe(request, mapIssue, filterStatus('open'))(ISSUE_URL);
+const closeStatusList = await pipe(request, mapIssue, filterStatus('close'))(ISSUE_URL);
 
 const setIssueTpl = (issueCount) => go(issueCount, getIssueTpl, setTpl);
 
 const reduceIssueItemTpl = (list) => list.reduce((acc, curr) => (acc += getIssueItemTpl(curr)), '');
 const setIssueListTpl = (list) => go(list, reduceIssueItemTpl, setTpl);
 
-const addToggleCountEvent = (setListTpl, $target, $nontarget) => {
-  $target.addEventListener('click', () => {
-    toggleCountBtn(setListTpl, $target, $nontarget);
-  });
+const setInitialIssueTpl = (issueCount) => {
+  setIssueTpl(issueCount)($('#app'));
+  setIssueListTpl(openStatusList)($('#issues'));
 };
 
 const toggleCountBtn = (setListTpl, $focused, $unfocused) => {
@@ -33,9 +40,10 @@ const toggleCountBtn = (setListTpl, $focused, $unfocused) => {
   $unfocused.style.fontWeight = 'normal';
 };
 
-const setInitialIssueTpl = (issueCount) => {
-  setIssueTpl(issueCount)($('#app'));
-  setIssueListTpl(openStatusList)($('#issues'));
+const addToggleCountEvent = (setListTpl, $target, $nontarget) => {
+  $target.addEventListener('click', () => {
+    toggleCountBtn(setListTpl, $target, $nontarget);
+  });
 };
 
 const addToggleCountEvents = () => {
@@ -43,7 +51,7 @@ const addToggleCountEvents = () => {
   addToggleCountEvent(setIssueListTpl(openStatusList), $('.open-count'), $('.close-count'));
 };
 
-export const setIssueOnDocument = async () => {
+export const setIssueOnDocument = () => {
   setInitialIssueTpl({ openCount: openStatusList.length, closeCount: closeStatusList.length });
   addToggleCountEvents();
 };
