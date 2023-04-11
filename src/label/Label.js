fc28fd36a6cfd3dd0f0c0ffe125312d4f676936d
김양원 <yangwon.kim@woowahan.com>
2023-04-11 16:07:55 +0900

    feat: Label 컴포넌트 생성

---
+++ src/label/Label.js
@@ -0,0 +1,34 @@
+import { getLabelTpl } from '../tpl';
+import { $ } from '../utils/dom';
+import LabelList from './views/LabelList';
+import LabelCreator from './views/LabelCreator';
+import LabelListModel from './models/LabelListModel';
+import LabelCreatorModel from './models/LabelCreatorModel';
+
+class Label {
+  #model = null;
+  constructor({ model }) {
+    // TODO: 전역 상태
+    this.#model = model;
+    this.#model.subscribe(this.render.bind(this));
+    this.render();
+    this.#addEventOfCreator();
+  }
+
+  #addEventOfCreator() {
+    $('.new-label-button').addEventListener('click', (e) => {
+      e.preventDefault();
+      this.#model.isNewLabelClicked = true;
+    });
+  }
+
+  render() {
+    $('#app').innerHTML = getLabelTpl();
+
+    new LabelList({ listModel: LabelListModel });
+    this.#model.isNewLabelClicked &&
+      new LabelCreator({ creatorModel: LabelCreatorModel, listModel: LabelListModel });
+  }
+}
+
+export default Label;
---
+++ src/label/index.js
@@ -0,0 +1,4 @@
+import Label from './Label';
+import LabelModel from './models/LabelModel';
+
+export default new Label({ model: new LabelModel() });
