a8f2efdaa6738169ecd2da8b698f159ac9b48d10
김양원 <yangwon.kim@woowahan.com>
2023-04-12 19:58:05 +0900

    chore: CreateButton 이동

--- src/label/views/LabelCreator/CreateButton.js
+++ src/label/views/LabelCreator/CreateButton.js
@@ -2,17 +2,16 @@ import { $ } from '../../../utils';
 
 class CreateButton {
   constructor({ listModel, creatorModel }) {
-    this.render({ listModel, creatorModel });
+    this.#init({ listModel, creatorModel });
   }
 
-  render({ listModel, creatorModel }) {
+  #init({ listModel, creatorModel }) {
     const $button = $('#label-create-button');
     $button.disabled = false;
     $button.classList.remove('opacity-50');
     $button.addEventListener('click', (e) => {
       e.preventDefault();
       listModel.addLabel(creatorModel.newLabel);
-      debugger;
     });
   }
 }
