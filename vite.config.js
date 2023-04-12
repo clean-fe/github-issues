91011c9c8b811388391c93b6c789a97a422dc606
sbpark88 <saebyulpark88@gmail.com>
2023-04-12 21:40:05 +0900

    Chore: 일부 플러그인 추가 설치
    - vite-plugin-babel
    - @babel/plugin-proposal-decorators
    - @babel/plugin-proposal-private-methods
    - @babel/plugin-proposal-class-properties
    
    node express 서버를 띄우던 msw 를 띄우던 해야할
    것 같다. root directory 가 /src 로 변경되어
    root 와 같은 레벨에 존재해버리는 'data-sources'
    에 접근을 못 하는 문제가 발생해 src 안으로 같이
    옮겼는데 맘에 안 든다.

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import {defineConfig} from 'vite';
+import babel from 'vite-plugin-babel';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: './dist',
+  },
+  plugins: [
+    babel(),
+  ],
+});
+
