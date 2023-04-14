ac9afce26a24ec75c77aaf40f35d9130937ec912
Jiwon Kim <lost731@naver.com>
2023-04-15 00:06:31 +0900

    chore: msw 셋업

--- src/core/observer.js
+++ src/core/observer.js
@@ -1,30 +1,3 @@
-let currentObserver = null;
-
-export const observe = (fn) => {
-  currentObserver = fn;
-  fn();
-  currentObserver = null;
-};
-
-export const observable = (store) => {
-  Object.keys(store).forEach((key) => {
-    let val = store[key];
-    const observers = new Set();
-
-    Object.defineProperty(store, key, {
-      get() {
-        if (currentObserver) observers.add(currentObserver);
-        return val;
-      },
-      set(nextVal) {
-        val = nextVal;
-        observers.forEach((fn) => fn());
-      },
-    });
-  });
-  return store;
-};
-
 export const create = (createState) => {
   let state;
   const observers = new Set();
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import setIssueOnDocument from './issue';
 import { Label } from './label/Label.js';
 
+import { worker } from './mocks/browser.js';
+worker.start();
+
 setIssueOnDocument();
 
 new Label();
