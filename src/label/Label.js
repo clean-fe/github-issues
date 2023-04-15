c6ceb2a23c961881610407d7516170f23e946018
Jiwon Kim <lost731@naver.com>
2023-04-15 17:31:18 +0900

    feat: label components들을 Label 버튼 누를 때 dynamic import

--- src/label/Label.js
+++ src/label/Label.js
@@ -1,7 +1,30 @@
-import { LabelButton } from './components.js';
+import { $ } from '../utils/index.js';
+import { getLabelTpl } from '../tpl.js';
+import { labelFormStore, labelListStore } from '../store/labelStore.js';
 
-export class Label {
-  constructor() {
-    new LabelButton().addEvent();
+export class LabelButton {
+  addEvent() {
+    const labelBtn = $('#label-btn');
+    labelBtn.addEventListener('click', () => {
+      $('#app').innerHTML = getLabelTpl();
+      this.initLabelPage();
+    });
+  }
+  async initLabelPage() {
+    const { LabelList, NewLabelBtn, LabelForm } = await import(
+      './components.js'
+    );
+    const labelForm = LabelForm();
+    labelForm.init();
+    new LabelList({
+      setList: labelListStore.setLabelList,
+      subscribe: labelListStore.subscribe,
+    });
+    new NewLabelBtn({
+      subscribe: labelFormStore.subscribe,
+      toggleFormOpened: labelFormStore.toggleFormOpened,
+      revealForm: labelForm.revealForm,
+      hideForm: labelForm.hideForm,
+    });
   }
 }
--- src/label/components.js
+++ src/label/components.js
@@ -2,31 +2,7 @@ import { $, request } from '../utils';
 import { getLabelItemTpl, getLabelTpl } from '../tpl.js';
 import { labelFormStore, labelListStore } from '../store/labelStore.js';
 
-export class LabelButton {
-  addEvent() {
-    const labelBtn = $('#label-btn');
-    labelBtn.addEventListener('click', () => {
-      $('#app').innerHTML = getLabelTpl();
-      this.initLabelPage();
-    });
-  }
-  initLabelPage() {
-    const labelForm = LabelForm();
-    labelForm.init();
-    new LabelList({
-      setList: labelListStore.setLabelList,
-      subscribe: labelListStore.subscribe,
-    });
-    new NewLabelBtn({
-      subscribe: labelFormStore.subscribe,
-      toggleFormOpened: labelFormStore.toggleFormOpened,
-      revealForm: labelForm.revealForm,
-      hideForm: labelForm.hideForm,
-    });
-  }
-}
-
-const LabelForm = (selector = '#new-label-form') => {
+export const LabelForm = (selector = '#new-label-form') => {
   const $target = $(selector);
   return {
     $target,
