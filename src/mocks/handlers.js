473fff13818874929da536ccb48adc7b6d0d7abf
sa02045 <sa02045@naver.com>
2023-04-18 00:28:03 +0900

    feat: label mock Data

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -118,3 +118,36 @@ const issues = [
     "comments-count": 0,
   },
 ];
+
+const labels = [
+  {
+    name: "bug",
+    color: "bfdadc",
+    description: "this is red",
+  },
+  {
+    name: "documentation",
+    color: "0075ca",
+    description: "this is documetation",
+  },
+  {
+    name: "enhancement",
+    color: "a2eeef",
+    description: "this is enhancement",
+  },
+  {
+    name: "question",
+    color: "d876e3",
+    description: "this is question",
+  },
+  {
+    name: "invalid",
+    color: "e4e669",
+    description: "this is not valid",
+  },
+  {
+    name: "duplicate",
+    color: "cfd3d7",
+    description: "this is dulicate",
+  },
+];
