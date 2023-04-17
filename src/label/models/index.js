c219f01e9f919b0dc17783a94fb9cb1489b588ec
김양원 <yangwon.kim@woowahan.com>
2023-04-18 01:26:53 +0900

    chore: 기존 Model 제거

--- src/label/models/index.js
+++
@@ -1,5 +0,0 @@
-import LabelModel from './LabelModel';
-import LabelListModel from './LabelListModel';
-import LabelCreatorModel from './LabelCreatorModel';
-
-export { LabelModel, LabelListModel, LabelCreatorModel };
