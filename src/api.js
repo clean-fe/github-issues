07f0947a46f1ab81dd73180a5d59934663d385ea
sa02045 <sa02045@naver.com>
2023-04-17 23:24:38 +0900

    feat: label api 추가

---
+++ src/api.js
@@ -0,0 +1,41 @@
+export async function fetchGetLabels() {
+  try {
+    const res = await fetch("/labels");
+    if (res.ok) {
+      return await res.json();
+    }
+    throw Error("fetch error");
+  } catch (e) {
+    throw Error(e);
+  }
+}
+
+export async function fetchPostLabels(data) {
+  try {
+    const res = await fetch("/labels", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (res.ok) {
+      return await res.json();
+    }
+    throw Error("fetch error");
+  } catch (e) {
+    throw Error(e);
+  }
+}
+
+export async function fetchDelayLabels() {
+  try {
+    const res = await fetch("/labels-delay");
+    if (res.ok) {
+      return await res.json();
+    }
+    throw Error("fetch error");
+  } catch (e) {
+    throw Error(e);
+  }
+}
