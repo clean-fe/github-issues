b0c6597064178a14607bcec296e19161ab514c0b
김양원 <yangwon.kim@woowahan.com>
2023-04-18 01:26:02 +0900

    fix: navigate 수정

--- src/navigate.js
+++ src/navigate.js
@@ -9,11 +9,7 @@ const router = () => {
   const { pathname } = window.location;
   switch (pathname) {
     case ROUTER_PATH.LABEL:
-      Promise.all([import('./label/views/Label'), import('./label/models/LabelModel')]).then(
-        ([{ default: Label }, { default: LabelModel }]) => {
-          new Label({ model: LabelModel });
-        },
-      );
+      import('./label/views/Label').then(({ default: Label }) => new Label());
       break;
     case ROUTER_PATH.ISSUE:
     case ROUTER_PATH.ROOT: // TODO: label 개발 후 issue 페이지로 변경
