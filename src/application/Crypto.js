27b25fe407c9d86f25d8e5f31e6d66bc638c9098
sbpark88 <saebyulpark88@gmail.com>
2023-04-18 00:32:56 +0900

    Add: Crypto.js

---
+++ src/application/Crypto.js
@@ -0,0 +1,7 @@
+export default async function sha256(message) {
+  const buffer = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', buffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
