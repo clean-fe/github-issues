2c2b37b6d80bdedebb36a0f8cf78fd3063b0c626
onlinekkabchi <onlinekkabchi@gmail.com>
2023-04-19 20:09:09 +0900

    data-sources/labels.json 데이터 넣어서 초기화면 띄우기 성공

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { getLabelTpl } from "./tpl.js";
-
-import { Model } from "./model.js";
+import { View } from "./view.js";
 
 const app = document.querySelector("#app");
 app.insertAdjacentHTML("beforeend", getLabelTpl());
 
-function main() {
-  const sample = new Model();
-  app.insertAdjacentHTML("beforeend", sample.createLabelItem());
+const view = new View();
 
-  return;
+async function main() {
+  await view.getLabelData();
+  const labelItems = view.render().join("");
+  app.insertAdjacentHTML("beforeend", labelItems);
 }
 
 main();
