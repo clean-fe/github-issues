494485e91292ad4e0fe90c58884103660a471620
onlinekkabchi <onlinekkabchi@gmail.com>
2023-04-20 19:41:28 +0900

    server.js 가지고 삽질

---
+++ src/proxy.js
@@ -0,0 +1,19 @@
+class Secret {
+  #secret;
+  constructor(secret) {
+    this.#secret = secret;
+  }
+  get secret() {
+    return this.#secret.replace(/\d+/, "[REDACTED]");
+  }
+}
+
+const aSecret = new Secret("123456");
+
+const proxy = new Proxy(aSecret, {
+  get(target, prop, receiver) {
+    // By default, it looks like Reflect.get(target, prop, receiver)
+    // which has a different value of `this`
+    return target[prop];
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
+import { defineConfig } from "vite";
+
 export default defineConfig({
   server: {
     proxy: {
       // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
       "/foo": "http://localhost:4567",
-      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-      "/api": {
-        target: "http://jsonplaceholder.typicode.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+
       // with RegEx: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
       "^/fallback/.*": {
         target: "http://jsonplaceholder.typicode.com",
