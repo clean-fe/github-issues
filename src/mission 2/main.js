dc17d6cfc1356d53c463bd4760844de6a3e7c0a5
sa02045 <sa02045@naver.com>
2023-04-20 20:02:17 +0900

    feat: newLabel 분리

--- src/mission 2/main.js
+++ src/mission 2/main.js
@@ -2,8 +2,10 @@ import { labelModel } from "./Model.js";
 import { ViewModel } from "./ViewModel.js";
 import { getLabelTpl } from "./tpl.js";
 import { getRandomColor } from "../utils/getRandomColor.js";
+import { safeLocalStorage } from "../utils/storage.js";
 
 export function main() {
+
   new ViewModel({
     element: "#app",
     view: getLabelTpl,
@@ -21,41 +23,52 @@ export function main() {
         handler(e) {
           e.preventDefault();
           this.model.toggleIsLabelFormHidden();
-        },
-      },
-      {
-        target: "#label-name-input",
-        eventName: "input",
-        handler(e) {
-          this.model.data.labelName = e.target.value;
-        },
-      },
-      {
-        target: "#label-description-input",
-        eventName: "input",
-        handler(e) {
-          this.model.data.labelDescription = e.target.value;
-        },
-      },
-      {
-        target: "#new-label-color",
-        eventName: "click",
-        handler() {
-          this.model.data.color = getRandomColor();
-        },
-      },
-      {
-        target: "#label-create-button",
-        eventName: "click",
-        handler(e) {
-          e.preventDefault();
-          const newLabel = {
-            name: this.model.data.labelName,
-            color: this.model.data.color.replace(/^#/, ""),
-            description: this.model.data.labelDescription,
-          };
 
-          this.model.fetchCreateLabel(newLabel);
+          import("../NewLabel.js").then(({ getNewLabelTpl }) => {
+            new ViewModel({
+              element: ".new-label-container",
+              view: getNewLabelTpl,
+              model: labelModel,
+              isRoot: false,
+              mounted() {},
+              events: [
+                {
+                  target: "#label-name-input",
+                  eventName: "input",
+                  handler(e) {
+                    this.model.data.labelName = e.target.value;
+                  },
+                },
+                {
+                  target: "#label-description-input",
+                  eventName: "input",
+                  handler(e) {
+                    this.model.data.labelDescription = e.target.value;
+                  },
+                },
+                {
+                  target: "#new-label-color",
+                  eventName: "click",
+                  handler() {
+                    this.model.data.color = getRandomColor();
+                  },
+                },
+                {
+                  target: "#label-create-button",
+                  eventName: "click",
+                  handler(e) {
+                    e.preventDefault();
+                    const newLabel = {
+                      name: this.model.data.labelName,
+                      color: this.model.data.color.replace(/^#/, ""),
+                      description: this.model.data.labelDescription,
+                    };
+                    this.model.fetchCreateLabel(newLabel);
+                  },
+                },
+              ],
+            });
+          });
         },
       },
       {
@@ -63,10 +76,8 @@ export function main() {
         eventName: "click",
         async handler(e) {
           e.preventDefault();
-
           const controller = new AbortController();
           const signal = controller.signal;
-
           try {
             this.model.data.controllers.push(controller);
             this.model.fetchDelayLabelList(signal);
