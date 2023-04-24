b57a10aec9dc98d55571a600f14c2ed0573eba7b
Jiwon Kim <lost731@naver.com>
2023-04-24 20:09:26 +0900

    chore: set up vitest

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import setIssueOnDocument from './issue';
 import { LabelButton } from './label/Label.js';
 
-import { worker } from './mocks/browser.js';
+import { worker } from './__mocks__/browser.js';
 worker.start();
 
 setIssueOnDocument();
