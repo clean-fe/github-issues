54c0e1e45d21868034d581779aa6aa0961587e0e
Yangseungchan <uvzone@naver.com>
2023-04-25 17:40:25 +0900

    feat: post 요청 공통 함수 정의

--- src/api/fetcher.js
+++ src/api/fetcher.js
@@ -1,14 +1,33 @@
 import { ApiError } from './errors';
 
-const get = async (url) => {
+const get = async (url, errorMessage = 'GET 요청에서 에러가 발생했습니다.') => {
   try {
     const res = await fetch(url);
     return res.json();
   } catch (err) {
-    throw new ApiError('GET 요청에서 에러가 발생했습니다.', err.status, err.message);
+    throw new ApiError(errorMessage, err.status, err.message);
   }
 };
 
-const post = async (url) => {};
+const post = async (url, body = {}, errorMessage = 'POST 요청에서 에러가 발생했습니다.') => {
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+    });
+    if (!res.ok) {
+      throw new Error(errorMessage, {
+        cause: {
+          statusCode: res.status,
+          details: res.statusText,
+        },
+      });
+    }
+    return res;
+  } catch (e) {
+    const { statusCode, details } = e.cause;
+    throw new ApiError(e.message, statusCode, details);
+  }
+};
 
 export { get, post };
