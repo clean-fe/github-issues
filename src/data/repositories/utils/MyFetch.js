8fe025b52fc2b360422a7817cd34ebf1a8c56ca1
sbpark88 <saebyulpark88@gmail.com>
2023-04-27 03:36:16 +0900

    Update: Make MyFetch do handling http errors

--- src/data/repositories/utils/MyFetch.js
+++ src/data/repositories/utils/MyFetch.js
@@ -18,16 +18,32 @@ const getOptions = createOptions('GET')
 const postOptions = createOptions('POST')
 const putOptions = createOptions('PUT')
 const deleteOptions = createOptions('DELETE')
-const customFetch = options => url => {
-  return async () => {
-    if (url) {
-      const response = await fetch(url, options)
-      return await response.json()
+const customFetch = options => async url => {
+  if (!url) return null
+  try {
+    const response = await fetch(url, options)
+    if (!response.ok) throw new HTTPError(response.status, response.statusText)
+
+    return response.json()  // Promise 를 반환하므로 await 없어도 됨. 상위 호출자가 resolve 상태가 필요할 경우 알아서 async await 처리
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      console.error('HTTP Error occurred: ', error)
+      throw error
     } else {
-      return null
+      console.error('Non-HTTP Error occurred: ', error)
+      throw error
     }
   }
 }
+
+class HTTPError extends Error {
+  constructor(status, statusText) {
+    super(`HTTP Error ${status}: ${statusText}`)
+    this.status = status
+    this.statusText = statusText
+  }
+}
+
 
 export {
   getOptions,
