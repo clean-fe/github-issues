7892742317dde157c8a83245daef7471de2e8b9e
김양원 <yangwon.kim@woowahan.com>
2023-04-27 23:04:13 +0900

    fix: 코드리뷰 반영: 테스트설명 수정, 검증문 긍정형으로 변경

--- src/issue/__tests__/issue.spec.js
+++ src/issue/__tests__/issue.spec.js
@@ -1,10 +1,10 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { HANDLER_ISSUES } from '../../__mock_data__/handlers';
 import { server } from '../../__mock_data__/server';
 import setIssueOnDocument from '../issue';
 
 describe('issue/issue', () => {
-  it('issue 화면이 노출된다', async () => {
+  it('데이터 응답이 성공 + issue 목록이 있으면, issue 목록이 나열된 화면이 노출된다', async () => {
     // given
     server.use(HANDLER_ISSUES.getSuccess);
 
@@ -13,6 +13,6 @@ describe('issue/issue', () => {
 
     // then
     const sut = document.querySelector('#issue-wrapper');
-    expect(sut).not.toBeFalsy();
+    expect(sut).toBeDefined();
   });
 });
