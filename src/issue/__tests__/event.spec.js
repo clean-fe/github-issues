e1720042d56925dbf812fdb7905214aef46945d0
김양원 <yangwon.kim@woowahan.com>
2023-04-27 23:03:42 +0900

    fix: 코드리뷰 반영: bold처리 풀리는 검증 추가, 테스트코드 파라미터 타입 변경

--- src/issue/__tests__/event.spec.js
+++ src/issue/__tests__/event.spec.js
@@ -12,11 +12,21 @@ describe('issue/event', () => {
   });
 
   it.each([
-    ['closed', '.close-count', '.open-count', FIXTURE_CLOSE_STATUS_LIST],
-    ['open', '.open-count', '.close-count', FIXTURE_OPEN_STATUS_LIST],
+    {
+      state: 'closed',
+      targetSelector: '.close-count',
+      nonTargetSelector: '.open-count',
+      list: FIXTURE_CLOSE_STATUS_LIST,
+    },
+    {
+      state: 'open',
+      targetSelector: '.open-count',
+      nonTargetSelector: '.close-count',
+      list: FIXTURE_OPEN_STATUS_LIST,
+    },
   ])(
     '%s 누르면, 해당 상태의 라벨 목록이 노출된다',
-    (state, targetSelector, nonTargetSelector, list) => {
+    ({ state, targetSelector, nonTargetSelector, list }) => {
       // given
       const $target = document.querySelector(targetSelector);
       const $nonTarget = document.querySelector(nonTargetSelector);
@@ -37,11 +47,23 @@ describe('issue/event', () => {
     },
   );
   it.each([
-    ['closed', 'opens', '.close-count', '.open-count', FIXTURE_CLOSE_STATUS_LIST],
-    ['open', 'closed', '.open-count', '.close-count', FIXTURE_OPEN_STATUS_LIST],
+    {
+      target: 'closed',
+      nonTarget: 'opens',
+      targetSelector: '.close-count',
+      nonTargetSelector: '.open-count',
+      list: FIXTURE_CLOSE_STATUS_LIST,
+    },
+    {
+      target: 'open',
+      nonTarget: 'closed',
+      targetSelector: '.open-count',
+      nonTargetSelector: '.close-count',
+      list: FIXTURE_OPEN_STATUS_LIST,
+    },
   ])(
     `%s를 클릭하면, 해당 글자는 bold 처리 되고, %s 글자는 bold 처리가 풀린다`,
-    (target, nonTarget, targetSelector, nonTargetSelector, list) => {
+    ({ target, nonTarget, targetSelector, nonTargetSelector, list }) => {
       // given
       const $target = document.querySelector(targetSelector);
       const $nonTarget = document.querySelector(nonTargetSelector);
@@ -57,8 +79,10 @@ describe('issue/event', () => {
       $target.dispatchEvent(new window.MouseEvent('click'));
 
       // then
-      const sut = $target.classList.contains('font-bold');
-      expect(sut).toStrictEqual(true);
+      const sutOfTarget = $target.classList.contains('font-bold');
+      const sutOfNonTarget = $nonTarget.classList.contains('font-bold');
+      expect(sutOfTarget).toBe(true);
+      expect(sutOfNonTarget).toBe(false);
     },
   );
 });
